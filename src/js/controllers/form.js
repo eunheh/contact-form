83be1c4b33c49029ec079de3ba9bd47857353142
Grace Lee <eunheh@gmail.com>
2016-11-15 13:53:49 -0500

    form css complete

---
+++ src/js/controllers/form.js
@@ -0,0 +1,13 @@
+const URL = 'https://class-server.herokuapp.com/collections/glee-contacts';
+
+function FormController ($scope, $http) {
+  $scope.gifs = [];
+  $scope.errors = {};
+
+
+
+
+}
+
+FormController.$inject = ['$scope', '$http'];
+export { FormController };
